ef6dd3c7eef706eee0d8258b4e288e25e65883cd
Viachaslau Katsuba <vkatsubo@gmail.com>
2025-10-02 19:35:08 +0300

    v3

--- README.md
+++ README.md
@@ -39,21 +39,23 @@ Base usage
 
 Create `Module` (recommend to use `object`) and extends from `Leviathan` class
 
-Create fields using one functions:
+Create fields using these functions:
 
-- Use `by instanceOf` to create single-object-delegate (same instance upon every access)
-- Use `by lateInitInstance` to create instance-based late-init dependency (ps: you need to call `provides` method before
-  access)
-- Use `by factoryOf` to create factory-delegate (new instance upon each access)
+- Use `by instanceOf(keepAlive){/**/}` to create instance dependency
+  - `keepAlive = true` : instance persists across different scopes
+  - `keepAlive = false`(default): instance is auto-closed when all scopes close
+- Use `by factoryOf(useCache)` to create factory dependency
+  - `useCache = true` (default): caches instances within the same scope
+  - `useCache = false`: creates new instance on each access
+- Use `by valueOf(value)` to create value dependency (returns the same value always)
 
-Both functions return a dependency provider instance and the type of field will be `Dependency<Type>`
+All functions return a `Dependency<Type>` instance.
 
-To retreive dependency use either `Module.dependency.get()` or define a property `val dep by Module.dependency`
 
-Simple case
+Example
 -----------
 
-Declare you dependencies
+Declare your dependencies
 
 ```kotlin
 class SampleRepository()
@@ -68,156 +70,45 @@ Create module
 
 ```kotlin
 object Module : Leviathan() {
-    val lazyRepository by instanceOf(::SampleRepository)
-    val nonLazyRepository by instanceOf(false, ::SampleRepository)
+    val autoCloseRepository by instanceOf { SampleRepository() }
+    val keepAliveRepository by instanceOf(keepAlive = true) { SampleRepository() }
     val repositoryWithParam by factoryOf { SampleRepositoryWithParam(1) }
-    val repositoryWithDependency by instanceOf { SampleRepositoryWithDependency(lazyRepository.get()) }
-    val interfaceRepo by instanceOf<SampleInterfaceRepo>(::SampleInterfaceRepoImpl)
+    val repositoryWithDependency by instanceOf { 
+        SampleRepositoryWithDependency(inject(autoCloseRepository)) 
+    }
+    val interfaceRepo by instanceOf<SampleInterfaceRepo> { SampleInterfaceRepoImpl() }
+    val constantValue by valueOf(42)
 }
 ```
 
 Dependencies usage:
 
 ```kotlin
-fun foo() {
-    val repo = Module.lazyRepository.get()
-    //...  
-}
+// view model
+class SomeVM(
+    dep1: Dependency<SampleRepository> = Module.autoCloseRepository,
+) : ViewModel() {
+    val dep1value = inject(dep1)
 
-class Model(
-    val dep1: SampleRepository = Module.lazyRepository.get()
-) {
-    val dep2: SampleRepository by Module.nonLazyRepository
-    //...
-}
-
-```
-
-Mutli-module case
------------------
-
-Interface based approach
-
-```kotlin
-// ----------Module 1-------------
-//Dependency 
-class Dep {
-    fun foo() {}
-}
-
-// ----------Module 2-------------
-// Dependency provider interface
-interface ICore {
-    val dep: Dependency<Dep>
-}
-
-// Dependency provider implementation
-internal class CoreImpl : Leviathan(), ICore {
-    override val dep by instanceOf { Dep() }
-}
-// Dependency provider accessor
-val Core: ICore = CoreImpl()
-
-// ----------Module 3-------------    
-// Usage
-fun boo() {
-    val dep by Core.dep
-} 
-   ```
-
-Simple approach
-
-```kotlin
-// ----------Module 1-------------
-//Dependency
-class Dep {
-    fun foo() {}
-}
-
-// ----------Module 2-------------
-// Dependency provider & accessor
-object Core : Leviathan() {
-    val dep by instanceOf { Dep() }
-}
-
-// ----------Module 3-------------
-// Usage
-fun boo() {
-    val dep by Core.dep
-}
-```
-
-Advanced case
--------------
-
-In order to create good & testable classes recommend to use advanced scenario
-
-1) declare dependencies
-    ```kotlin
-    class DataRepository //...
-    class ApiRepository //...
-    ```
-2) declare module interface (data/domain modules)
-    ```kotlin
-    interface DataModule {
-        val dataRepository: Dependency<DataRepository>
+    fun foo(){
+        val dep2 = inject(Module.interfaceRepo)
     }
-    
-    interface ApiModule {
-        val apiRepository: Dependency<ApiRepository>
-    }    
-    ```
-3) Create `AppModule` and inherit from interfaces(step #2) and `Leviathan`
-    ```kotlin
-    object AppModule : DataModule, ApiModule, Leviathan() {
-        override val dataRepository: Dependency<DataRepository> by instance(::DataRepository)
-        override val apiRepository: Dependency<ApiRepository> by instance(::ApiRepository)
-    }
-    ```
-4) Create Models (or any other classes) base on interfaces from step #2
-    ```kotlin
-    class Model(apiModule: ApiModule = AppModule){
-        val api: ApiRepository by apiModule.apiRepository
-   
-        fun foo(){/*...*/}
-    }
-    ```
-
-Now you can make tests and have easy way to mock your data:
-
-```kotlin
-@Test
-fun ModelTests() {
-    val model = Model(object : Leviathan(), ApiModule {
-        override val apiRepository by instanceOf { MyMockApiRepository() }
-    })
-    model.foo()
-
-    //-----or-----------
-
-    AppModule.apiRepository.overrideWith { MyMockApiRepository() }
-    val model = Model()
-    model.foo()
-}
-```
-
-Compose
--------------
-
-Dependencies access in compose code:
-```kotlin
-class Repository(){
-    fun foo(){}
 }
 
-object Module : Leviathan(){
-    val dependency by instanceOf { Repository() }
-}
-
+// compose
 @Composable
-fun SomeComposable(){
-    val dependency = leviathanInject { Module.dependency }
-    ///...
+fun ComposeWithDI() {
+    val repo1 = inject(Module.autoCloseRepository)
+    val repo2 = inject { Module.repositoryWithParam }
+    /*..*/
+}
+
+// random access
+fun foo() {
+    val scope = DIScope()
+    val repo1 = Module.autoCloseRepository.injectedIn(scope)
+    /*..*/
+    scope.close()
 }
 ```
 
--- leviathan/src/commonMain/kotlin/com/composegears/leviathan/Leviathan.kt
+++ leviathan/src/commonMain/kotlin/com/composegears/leviathan/Leviathan.kt
@@ -1,155 +1,116 @@
 package com.composegears.leviathan
 
+import kotlin.jvm.JvmStatic
 import kotlin.reflect.KProperty
 
-/**
- * Annotation to mark delicate APIs in Leviathan.
- */
-@Retention(AnnotationRetention.BINARY)
-@RequiresOptIn(level = RequiresOptIn.Level.WARNING, message = "Developed for testing purposes")
-public annotation class LeviathanDelicateApi
+// ---  Scope definition -------------------------------------------
 
-/**
- * Base service locator class.
- */
+public open class DIScope {
+    public companion object {
+        public val GLOBAL: DIScope = object : DIScope() {
+            override fun onClose(action: () -> Unit) = Unit
+        }
+    }
+
+    private val closeActions = mutableListOf<() -> Unit>()
+
+    internal open fun onClose(action: () -> Unit) {
+        closeActions += action
+    }
+
+    public fun close() {
+        closeActions.onEach { it() }
+        closeActions.clear()
+    }
+}
+
+public class DependencyInitializationScope internal constructor(
+    private val diScope: DIScope
+) {
+    public fun <T> inject(dependency: Dependency<T>): T = dependency.injectedIn(diScope)
+}
+
+// ---  Dependency definition --------------------------------------
+
+public interface Dependency<T> {
+    public fun injectedIn(scope: DIScope): T
+}
+
+// public, useful for testing
+public class ValueDependency<T>(
+    private val value: T
+) : Dependency<T> {
+    override fun injectedIn(scope: DIScope): T = value
+}
+
+internal class FactoryDependency<T>(
+    private val useCache: Boolean,
+    private val factory: DependencyInitializationScope.() -> T
+) : Dependency<T> {
+    private val scopedCache = mutableMapOf<DIScope, T>()
+    override fun injectedIn(scope: DIScope): T =
+        if (useCache) {
+            scopedCache.getOrElse(scope) {
+                val instance = factory(DependencyInitializationScope(scope))
+                scopedCache[scope] = instance
+                scope.onClose {
+                    scopedCache.remove(scope)
+                }
+                instance
+            }
+        } else factory(DependencyInitializationScope(scope))
+}
+
+internal class InstanceDependency<T>(
+    private val keepAlive: Boolean,
+    private val factory: DependencyInitializationScope.() -> T
+) : Dependency<T> {
+
+    private var scopes = mutableSetOf<DIScope>()
+    private var instance: T? = null
+
+    override fun injectedIn(scope: DIScope): T {
+        if (!keepAlive && scope !in scopes) {
+            scopes.add(scope)
+            scope.onClose {
+                scopes.remove(scope)
+                if (scopes.isEmpty()) {
+                    instance = null
+                }
+            }
+        }
+        if (instance == null) {
+            instance = factory(DependencyInitializationScope(scope))
+        }
+        return instance!!
+    }
+}
+
+// ---  Module definition ------------------------------------------
+
 public abstract class Leviathan {
-    public companion object;
-
-    /**
-     * Creates a factory-based dependency provider.
-     *
-     * @param provider dependency provider.
-     * @return dependency provider instance.
-     */
-    protected fun <T> factoryOf(provider: () -> T): DependencyProvider<Dependency<T>> =
-        DependencyProvider(Factory(provider))
-
-    /**
-     * Creates a lazy instance-based dependency provider.
-     *
-     * @param provider dependency provider.
-     * @return dependency provider instance.
-     */
-    protected fun <T> instanceOf(provider: () -> T): DependencyProvider<Dependency<T>> =
-        instanceOf(true, provider)
-
-    /**
-     * Creates an instance-based dependency provider.
-     *
-     * @param lazy whether the instance should be lazily initialized.
-     * @param provider dependency provider.
-     * @return dependency provider instance.
-     */
-    protected fun <T> instanceOf(lazy: Boolean, provider: () -> T): DependencyProvider<Dependency<T>> =
-        if (lazy) DependencyProvider(Instance(provider))
-        else provider().let { factoryOf { it } }
-
-    /**
-     * Creates an instance-based late-init dependency provider.
-     *
-     * Call [LateInitDependency.provides] to set the provider.
-     *
-     * @return dependency provider instance.
-     */
-    protected fun <T> lateInitInstance(): DependencyProvider<LateInitDependency<T>> =
-        DependencyProvider(LateInitInstance())
-
-    protected operator fun <P, T : Dependency<P>> DependencyProvider<T>.getValue(
-        di: Any?,
-        property: KProperty<*>
-    ): T = dependency
-}
-
-// ---------------------------------API---------------------------------
-
-/**
- * Abstract class representing a dependency.
- *
- * @param T the type of the dependency.
- */
-public abstract class Dependency<T> internal constructor() {
-    /**
-     * @return the dependency instance.
-     */
-    public abstract fun get(): T
-
-    public operator fun getValue(thisRef: Any?, property: KProperty<*>): T = get()
-
-    /**
-     * Overrides the current dependency.
-     *
-     * @param provider new dependency provider
-     */
-    @LeviathanDelicateApi
-    public abstract fun overrideWith(provider: (() -> T)?)
-}
-
-/**
- * Class representing a late-initialized dependency.
- *
- * @param T The type of the dependency.
- */
-public abstract class LateInitDependency<T> internal constructor() : Dependency<T>() {
-    /**
-     * Sets the provider for the dependency.
-     *
-     * @param provider dependency provider.
-     */
-    public abstract fun provides(provider: () -> T)
-}
-
-/**
- * Dependency provider delegate
- *
- * @param T The type of the dependency.
- * @property dependency The dependency instance.
- */
-@Suppress("UseDataClass")
-public class DependencyProvider<T : Dependency<*>> internal constructor(
-    internal val dependency: T
-)
-
-// ---------------------------------Implementation---------------------------------
-
-internal class Instance<T>(provider: () -> T) : Dependency<T>() {
-    private var oValue: T? = null
-    private val dependency by lazy(provider)
-
-    override fun get(): T = oValue ?: dependency
-
-    @OptIn(LeviathanDelicateApi::class)
-    override fun overrideWith(provider: (() -> T)?) {
-        oValue = provider?.invoke()
-    }
-}
-
-internal class LateInitInstance<T> : LateInitDependency<T>() {
-    private var provider: (() -> T)? = null
-    private var oValueProvider: (() -> T)? = null
-
-    override fun provides(provider: () -> T) {
-        this.provider = provider
+    public companion object Companion {
+        @JvmStatic
+        protected operator fun <T> Dependency<T>.getValue(
+            iRef: Leviathan,
+            property: KProperty<*>
+        ): Dependency<T> = this
     }
 
-    override fun get(): T =
-        if (oValueProvider != null) oValueProvider!!()
-        else provider?.invoke() ?: error("LateInitInstance is not initialized")
+    protected fun <T> valueOf(
+        value: T
+    ): Dependency<T> =
+        ValueDependency(value)
 
-    @LeviathanDelicateApi
-    override fun overrideWith(provider: (() -> T)?) {
-        oValueProvider = provider
-    }
-}
+    protected fun <T> factoryOf(
+        useCache: Boolean = true,
+        factory: DependencyInitializationScope.() -> T
+    ): Dependency<T> =
+        FactoryDependency(useCache, factory)
 
-internal class Factory<T>(private val provider: () -> T) : Dependency<T>() {
-    private var oValueProvider: (() -> T)? = null
-
-    override fun get(): T =
-        if (oValueProvider != null) oValueProvider!!()
-        else provider()
-
-    @OptIn(LeviathanDelicateApi::class)
-    override fun overrideWith(provider: (() -> T)?) {
-        oValueProvider = provider
-    }
+    protected fun <T> instanceOf(
+        keepAlive: Boolean = false,
+        factory: DependencyInitializationScope.() -> T
+    ): Dependency<T> =
+        InstanceDependency(keepAlive, factory)
 }
